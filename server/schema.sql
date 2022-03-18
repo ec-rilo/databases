b328f816c7521d9586ede498ac4969f954c36ef0
Edgar Carrillo <ecarrillo046@gmail.com>
2022-03-17 22:55:25 -0700

    Complete designing multi-table schema

--- server/schema.sql
+++ server/schema.sql
@@ -2,13 +2,21 @@ CREATE DATABASE chat;
 
 USE chat;
 
+CREATE TABLE users (
+  Id int NOT NULL AUTO_INCREMENT,
+  Roomname varchar(25) NOT NULL,
+  PRIMARY KEY (Id)
+);
+
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  Id int NOT NULL AUTO_INCREMENT,
+  Text varchar(25) NOT NULL,
+  UsersId int,
+  PRIMARY KEY (Id),
+  FOREIGN KEY (UsersId) REFERENCES users(Id)
 );
 
 /* Create other tables and define schemas for them here! */
-
-
 
 
 /*  Execute this file from the command line by typing:
